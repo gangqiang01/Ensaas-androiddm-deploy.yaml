1cec9a8253ff529b39e8889ba4588671bf7bf403
gangqiang.sun(孙刚强) <gangqiang.sun@advantech.com.cn>
2020-06-02 17:26:59 +0800

    modify Dockerfile, chart resource

--- helm/BuildIMG/androiddm/build-docker-image/ready.sh
+++ helm/BuildIMG/androiddm/build-docker-image/ready.sh
@@ -3,5 +3,5 @@ rm m2mserver.jar
 cp /home/gangqiangsun/MYPROJECT/BACKEND/m2m-core-ensaas/leshan-mqtt-server-demo/target/leshan-mqtt-server-demo-1.0.0-ADV-jar-with-dependencies.jar m2mserver.jar
 ls -al
 rm -r ./androidDM/*
-cp -r /home/gangqiangsun/MYPROJECT/FRONTEND/m2mlink_wp/dist/* ./androidDM/
+cp -r /home/gangqiangsun/MYPROJECT/FRONTEND/m2mlink-ensaas/dist/* ./androidDM/
 ls -al ./androidDM/
